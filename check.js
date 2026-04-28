99b4e1c3fa30547d00237530fb36f6342aad93fe
Chaitali Mhetras <chaitali1105@gmail.com>
2026-04-28 15:15:53 +0530

    chore: cleanup test files

--- check.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const code = fs.readFileSync('bundle.js', 'utf8');
-const match = code.match(/baseURL:\s*['"]([^'"]+)['"]/);
-console.log('baseURL is:', match ? match[1] : 'not found');
